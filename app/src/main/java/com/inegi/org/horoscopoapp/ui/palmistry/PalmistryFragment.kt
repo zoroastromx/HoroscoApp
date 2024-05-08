c1279157f0d6f55316425c053e212fa072c6c4e9
alfredo <alfredo.marin@inegi.org.mx>
2024-05-08 12:33:53 -0600

    se crea el fragment de palmistry

---
+++ app/src/main/java/com/inegi/org/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,26 @@
+package com.inegi.org.horoscopoapp.ui.palmistry
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.inegi.org.horoscopoapp.R
+import com.inegi.org.horoscopoapp.databinding.FragmentLuckBinding
+import com.inegi.org.horoscopoapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_palmistry, container, false)
+    }
+
+}
