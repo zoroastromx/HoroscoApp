cf5e09279818076ee1f64a83c4d48d139d665e33
alfredo <alfredo.marin@inegi.org.mx>
2024-05-09 13:12:55 -0600

    se optimizan los imports

--- app/src/main/java/com/inegi/org/horoscopoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/inegi/org/horoscopoapp/ui/home/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.inegi.org.horoscopoapp.ui.home
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
-import androidx.navigation.NavHost
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.inegi.org.horoscopoapp.R
