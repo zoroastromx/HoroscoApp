a02c728a80076d562f173be44968803d5f18a431
alfredo <alfredo.marin@inegi.org.mx>
2024-05-08 21:12:27 -0600

    se pone en mayúscula la "l" en el nombre del archivo

--- app/src/main/java/com/inegi/org/horoscopoapp/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/inegi/org/horoscopoapp/ui/luck/LuckFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.inegi.org.horoscopoapp.R
-import com.inegi.org.horoscopoapp.databinding.FragmentHoroscopoBinding
 import com.inegi.org.horoscopoapp.databinding.FragmentLuckBinding
 
 class LuckFragment : Fragment() {
