7fe9403f16ca27d7c2d4f7d4215b77102774d7b1
alfredo <alfredo.marin@inegi.org.mx>
2024-05-09 13:11:58 -0600

    configuración del NavController para que navegue por los fragments

--- app/src/main/java/com/inegi/org/horoscopoapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/inegi/org/horoscopoapp/ui/home/MainActivity.kt
@@ -5,15 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.inegi.org.horoscopoapp.R
 import com.inegi.org.horoscopoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI(){
+        initNavigation()
+    }
+
+    private fun initNavigation(){
+
+        val navHost: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
+
     }
 }
