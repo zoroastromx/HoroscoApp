b8f94430dbff54604ab890bbe0185bba45b271d6
alfredo <alfredo.marin@inegi.org.mx>
2024-05-10 23:33:55 -0600

    comentando pq ahorita no se usa

--- app/src/main/java/com/inegi/org/horoscopoapp/ui/luck/LuckViewModel.kt
+++ app/src/main/java/com/inegi/org/horoscopoapp/ui/luck/LuckViewModel.kt
@@ -3,7 +3,7 @@ package com.inegi.org.horoscopoapp.ui.luck
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
-@HiltViewModel
-class LuckViewModel @Inject constructor():ViewModel(){
+//@HiltViewModel
+//class LuckViewModel @Inject constructor():ViewModel(){
 
-}
+//}
